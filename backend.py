83a56155299ce12a3a6d5198a7a0d466e53e3a13
Eden Ben David <edenbdv1@gmail.com>
2023-05-04 21:51:40 +0300

    Day 33: Fix the Weather degrees and unknown places exception.

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "d38782a85c73189b7e855ca377eaf388"
 
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     # get JSON data for any city for the next five days
     # at an interval of 3 hours
     url = f"http://api.openweathermap.org/data/2.5/forecast?" \
@@ -17,12 +17,8 @@ def get_data(place, forecast_days=None, kind=None):
     # we got 5 days, that mean 8*5 = 40 observations.
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Add title, text input, slider, selectbox, and subheader
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -10,12 +11,32 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # Get the temperature/sky data
+    try:
+        filtered_data = get_data(place, days)
+        # we can get a figure object from a data visualization library
+        # such as "Plotly" or "Bokeh".
 
-d, t = get_data(place, days, option)
+        if option == "Temperature":
+            # Create a temperature plot
+            temperatures = [dict["main"]["temp"]/10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
 
-# we can get a figure object from a data visualization library
-# such as "Plotly" or "Bokeh".
-# create a line graph, we need to give the axis array's objects
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-# this method gets a figure object as input
-st.plotly_chart(figure)
+            # create a line graph, we need to give the axis array's objects
+            figure = px.line(x=dates, y=temperatures,
+                             labels={"x": "Date", "y": "Temperature (C)"})
+            # this method gets a figure object as input
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths, width=115)
+
+    except KeyError:
+        st.write("That place does not exist.")
+
